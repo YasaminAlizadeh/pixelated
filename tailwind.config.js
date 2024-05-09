939e6cc154b5f577d0b626eccbd04bff494686c9
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2024-05-09 19:08:28 +0330

    style: added theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,16 @@ module.exports = {
   content: ["./src/**/*.tsx", "./src/**/*.css"],
   plugins: [require("@tailwindcss/forms")],
   theme: {
+    extend: {
+      colors: {
+        dark: "#131419",
+        light: "#EFEFEF",
+        primary: "#6607FF",
+        secondary: "#595973",
+        "accent--pink": "#C4294C",
+        "accent--orange": "#DC4A24",
+        "accent--gray": "#94a3b8",
+      },
+    },
   },
 };
