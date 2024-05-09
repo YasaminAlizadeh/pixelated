d23643050207736550b8a393c342c289df15b3e1
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2024-05-09 19:29:04 +0330

    feat: added context providers to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "app/app";
+import CanvasProvider from "./context/CanvasContext";
+import ColorProvider from "./context/ColorContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   <React.StrictMode>
+    <CanvasProvider>
+      <ColorProvider>
         <App />
+      </ColorProvider>
+    </CanvasProvider>
   </React.StrictMode>
 );
