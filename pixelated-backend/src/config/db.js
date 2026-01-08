7113eff50846cb517244e53bd58d40f2a59dd38f
Yasamin Alizadeh <itsyasaminalizadeh@gmail.com>
2026-01-08 16:48:32 +0330

    feat(production): prepare to deploy to vercel

--- pixelated-backend/src/config/db.js
+++ pixelated-backend/src/config/db.js
@@ -5,12 +5,21 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+const connectionConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+    };
+
+const pool = new Pool(connectionConfig);
 
 export default pool;
